c2a0b672f556f2f5bc640edce854efd2354ce242
bmlancien <bryan.lancien@rl-institut.de>
2021-10-18 15:00:16 +0200

    change table styling

--- layout.py
+++ layout.py
@@ -626,10 +626,10 @@ def get_graph_column():
                                     id=f"table_{graph}",
                                     export_format="csv",
                                     style_table={'overflowX': 'auto'},
-                                    style_header={'backgroundColor': 'rgb(30, 30, 30)'}, 
+                                    style_header={'backgroundColor': 'rgb(246, 248, 250)'}, 
                                     style_cell={
-                                        'backgroundColor': 'rgb(50, 50, 50)',
-                                        'color': 'white'
+                                        'backgroundColor': 'rgb(255, 255, 255)',
+                                        'color': 'rgb(50, 50, 50)'
                                     },
                                 )
                             )
