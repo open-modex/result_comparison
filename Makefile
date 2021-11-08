f0ff5c75d7c5de2e6142fc4aeae147e3ee107a48
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2021-11-08 17:02:41 +0100

    Added admin tools to delete color maps and labels

--- Makefile
+++ Makefile
@@ -4,3 +4,9 @@ create_db:
 
 delete_filter:
 	python3 -c "import manage; manage.delete_filter('$(FILTER)')"
+
+delete_color_map:
+	python3 -c "import manage; manage.delete_color_map('$(COLOR_MAP)')"
+
+delete_label:
+	python3 -c "import manage; manage.delete_label('$(LABEL)')"
--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-from app import server, db, Filter
+from app import server, db, Filter, Colors, Labels
 
 
 def create_all():
@@ -6,12 +6,24 @@ def create_all():
         db.create_all()
 
 
-def delete_filter(filter_name):
+def delete_entry(model, name):
     with server.app_context():
-        filter_ = Filter.query.filter_by(name=filter_name).first()
-        if filter_:
-            db.session.delete(filter_)
+        entry = model.query.filter_by(name=name).first()
+        if entry:
+            db.session.delete(entry)
             db.session.commit()
-            print(f"Successfully deleted filter '{filter_name}'.")
+            print(f"Successfully deleted entry '{name}'.")
         else:
-            print(f"No filter '{filter_name}' found.")
+            print(f"No entry '{name}' found.")
+
+
+def delete_filter(filter_name):
+    delete_entry(Filter, filter_name)
+
+
+def delete_color_map(color_map):
+    delete_entry(Colors, color_map)
+
+
+def delete_label(label):
+    delete_entry(Labels, label)
